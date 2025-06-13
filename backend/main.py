75561e8fd09ddd944cbaf66c5dd4b2bc2b85d031
Avinash Salian <avinash@neotrader.in>
2025-06-13 16:50:10 +0530

    Added frontend domain

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
         "http://127.0.0.1:3001",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
-        "*"
+        "https://salmon-plant-01df91e0f.6.azurestaticapps.net"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
